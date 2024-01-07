796ed0061e511d501548fac2e81f4a84a50effe6
JiWedetir <serafettindiyart@hotmail.com>
2024-01-07 22:06:17 +0100

    Add project files.

---
+++ SchiffeVersenken/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace SchiffeVersenken
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
