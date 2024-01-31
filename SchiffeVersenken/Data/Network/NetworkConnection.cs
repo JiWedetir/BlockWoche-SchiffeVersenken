744bb2fb9697c54f41907f7111ac4f56a65467d2
Jonas Gemeinder <Jonas.Gemeinder@hotmail.com>
2024-01-31 11:56:54 +0100

    Nachrichten von Netzwerkspieler werden verarbeitet

--- SchiffeVersenken/Data/Network/NetworkConnection.cs
+++ SchiffeVersenken/Data/Network/NetworkConnection.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Net;
-using System.Net.Sockets;
-using System.Text;
-using System.Threading;
+﻿using Newtonsoft.Json.Linq;
+using System.Diagnostics;
 
 namespace SchiffeVersenken.Data.Network
 {
@@ -25,10 +22,11 @@ namespace SchiffeVersenken.Data.Network
             isServer = true;
         }
 
-        public static async Task<bool> ConnectToServer(string ip)
+        public static async Task<bool> ConnectToServer(string ip, string userName, int boardSize)
         {
             client = new ClientAsync();
             await client.ConnectAsync(ip, port);
+            SendInitMessage(userName, boardSize);
             isServer = false;
             return client.IsClientConnected;
         }
@@ -45,9 +43,196 @@ namespace SchiffeVersenken.Data.Network
             }
         }
 
-        public static async void ReceiveMessageAsync(string message)
+        public static async Task<bool> ReceiveMessageAsync(string message)
         {
-            switch
+            bool valid = false;
+            try
+            {
+                var jObjectMessage = JObject.Parse(message);
+                string type = jObjectMessage.Properties().FirstOrDefault()?.Name;
+                switch (type)
+                {
+                    case "App":
+                        valid = await ReciveInitMessage(jObjectMessage);
+                        break;
+                    case "Board":
+                        valid = await ReciveBoardMessage(jObjectMessage);
+                        break;
+                    case "ShotAt":
+                        valid = await ReciveShotAtMessage(jObjectMessage);
+                        break;
+                    case "Message":
+                        valid = await ReciveTextMessage(jObjectMessage);
+                        break;
+                    default:
+                        valid = false;
+                        break;
+                }
+                return valid;
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e.Message);
+                return false;
+            }
+        }
+
+        private static async Task<bool> ReciveInitMessage(JObject message)
+        {
+            try
+            {
+                var app = message["App"].ToString();
+                int.TryParse(message["Version"].ToString(), out int version);
+                int.TryParse(message["BoardSize"].ToString(), out int boardSize);
+                var userName = message["UserName"].ToString();
+                // fragen ob gegen diesen spieler gespielt werden soll
+                return true;
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e.Message);
+                return false;
+            }
+        }
+
+        private static async Task<bool> ReciveBoardMessage(JObject message)
+        {
+            try
+            {
+                JArray boardArray = (JArray)message["Board"];
+                List<JArray> rows = boardArray.ToObject<List<JArray>>();
+                int[,] board = new int[rows.Count, rows[0].Count];
+
+                for (int i = 0; i < rows.Count; i++)
+                {
+                    for (int j = 0; j < rows[i].Count; j++)
+                    {
+                        board[i, j] = (int)rows[i][j];
+                    }
+                }
+                return true;
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e.Message);
+                return false;
+            }
+        }
+
+        private static async Task<bool> ReciveShotAtMessage(JObject message)
+        {
+            try
+            {
+                JObject shotAtObject = (JObject)message["ShotAt"];
+                int.TryParse(shotAtObject["X"].ToString(), out int x);
+                int.TryParse(shotAtObject["Y"].ToString(), out int y);
+                // schuss auf das board
+                return true;
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e.Message);
+                return false;
+            }
+        }
+
+        private static async Task<bool> ReciveTextMessage(JObject message)
+        {
+            try
+            {
+                var messageText = message["Message"].ToString();
+                // nachricht anzeigen im frontend
+                return true;
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e.Message);
+                return false;
+            }
+        }
+
+        private async static Task<bool> SendInitMessage(string userName, int boardSize)
+        {
+            try
+            {
+                JObject message = new JObject();
+                message.Add("App", "SchiffeVersenkenG&T");
+                message.Add("Version", 1);
+                message.Add("BoardSize", boardSize);
+                message.Add("UserName", userName);
+                SendMessageAsync(message.ToString());
+                return true;
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e.Message);
+                return false;
+            }
+        }
+
+        public async static Task<bool> SendBoardMessage(int[,] board)
+        {
+            try
+            {
+                JArray boardArray = new JArray();
+                for (int i = 0; i < board.GetLength(0); i++)
+                {
+                    JArray row = new JArray();
+                    for (int j = 0; j < board.GetLength(1); j++)
+                    {
+                        row.Add(board[i, j]);
+                    }
+                    boardArray.Add(row);
+                }
+                JObject message = new JObject();
+                message.Add("Board", boardArray);
+                SendMessageAsync(message.ToString());
+                return true;
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e.Message);
+                return false;
+            }
+        }
+
+        public async static Task<bool> SendShotAtMessage(int x, int y)
+        {
+            try
+            {
+                JObject shotAtObject = new JObject
+                {
+                    { "X", x },
+                    { "Y", y }
+                };
+                JObject message = new JObject
+                {
+                    { "ShotAt", shotAtObject }
+                };
+                SendMessageAsync(message.ToString());
+                return true;
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e.Message);
+                return false;
+            }
+        }
+
+        public async static Task<bool> SendTextMessage(string messageText)
+        {
+            try
+            {
+                JObject message = new JObject();
+                message.Add("Message", messageText);
+                SendMessageAsync(message.ToString());
+                return true;
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e.Message);
+                return false;
+            }
         }
     }
 }
