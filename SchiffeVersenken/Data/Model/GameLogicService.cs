40e6b475a07de1e186a36be8e9738edfdaa3754c
JiWedetir <serafettindiyart@hotmail.com>
2024-02-18 19:41:35 +0100

    Added GameEnd PopUp and Improved Game Page

---
+++ SchiffeVersenken/Data/Model/GameLogicService.cs
@@ -0,0 +1,15 @@
+﻿
+using SchiffeVersenken.Data.Model;
+
+namespace SchiffeVersenken.Data
+{
+    public class GameLogicService
+    {
+        public GameLogic Game { get; private set; }
+
+        public void CreateNewGame()
+        {
+            Game = new GameLogic();
+		}
+    }
+}
