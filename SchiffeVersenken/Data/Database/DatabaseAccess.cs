6d86bd133279a0727b1fb2095371916b4df54e48
Jonas Gemeinder <Jonas.Gemeinder@hotmail.com>
2024-02-20 19:35:45 +0100

    Error handling in der Datenbank

--- SchiffeVersenken/Data/Database/DatabaseAccess.cs
+++ SchiffeVersenken/Data/Database/DatabaseAccess.cs
@@ -9,74 +9,144 @@ namespace SchiffeVersenken.Data.Database
 
         async Task Init()
         {
-            if (Database is not null)
-                return;
-
-            Database = new SQLiteAsyncConnection(Constants.DatabasePath, Constants.Flags);
-            var result = await Database.CreateTableAsync<User>();
-            await Database.CreateTableAsync<Highscore>();
-            if (result == CreateTableResult.Created)
+            try
             {
-                await CreateDefaultUsers();
+                if (Database is not null)
+                    return;
+
+                Database = new SQLiteAsyncConnection(Constants.DatabasePath, Constants.Flags);
+                var result = await Database.CreateTableAsync<User>();
+                await Database.CreateTableAsync<Highscore>();
+                if (result == CreateTableResult.Created)
+                {
+                    await CreateDefaultUsers();
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
             }
         }
         internal async Task CreateDefaultUsers()
         {
-            User player = new User() { Name = "Player", PasswordHash="1234", Salt="1234" };
-            User dumm = new User() { Name = "Dummer_Computer", PasswordHash = "1234", Salt = "1234" };
-            User klug = new User() { Name = "Kluger_Computer", PasswordHash = "1234", Salt = "1234" };
-            User genial = new User() { Name = "Genialer_Computer", PasswordHash = "1234", Salt = "1234" };
-            await Database.InsertAllAsync(new List<User>() { player, dumm, klug, genial });
-            List<User> users = await Database.Table<User>().ToListAsync();
-            foreach (User user in users)
+            try
             {
-                Debug.WriteLine(user.Name);
+                User player = new User() { Name = "Player", PasswordHash="1234", Salt="1234" };
+                User dumm = new User() { Name = "Dummer_Computer", PasswordHash = "1234", Salt = "1234" };
+                User klug = new User() { Name = "Kluger_Computer", PasswordHash = "1234", Salt = "1234" };
+                User genial = new User() { Name = "Genialer_Computer", PasswordHash = "1234", Salt = "1234" };
+                await Database.InsertAllAsync(new List<User>() { player, dumm, klug, genial });
+                List<User> users = await Database.Table<User>().ToListAsync();
+                foreach (User user in users)
+                {
+                    Debug.WriteLine(user.Name);
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
             }
         }
         internal async Task<List<User>> GetUserNamesAsync()
         {
-            await Init();
-            List<User> users = await Database.QueryAsync<User>("SELECT Name FROM User WHERE Name NOT IN ('Player', 'Dummer_Computer', 'Kluger_Computer', 'Genialer_Computer')");
-            return users;
+            try
+            {
+                await Init();
+                List<User> users = await Database.QueryAsync<User>("SELECT Name FROM User WHERE Name NOT IN ('Player', 'Dummer_Computer', 'Kluger_Computer', 'Genialer_Computer')");
+                return users;
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+                return null;
+            }
         }
         internal async Task<User> GetUserAsync(int id)
         {
-            await Init();
-            return await Database.GetAsync<User>(id);
+            try
+            {
+                await Init();
+                return await Database.GetAsync<User>(id);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+                return null;
+            }
         }
         internal async Task<User> GetUserAsync(string name)
         {
-            await Init();
-            return await Database.Table<User>().Where(i => i.Name == name).FirstOrDefaultAsync();
+            try
+            {
+                await Init();
+                return await Database.Table<User>().Where(i => i.Name == name).FirstOrDefaultAsync();
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+                return null;
+            }
         }
         internal async Task<int> SaveUserAsync(User user)
         {
-            await Init();
-            if (user.Id != 0)
+            try
             {
-                return await Database.UpdateAsync(user);
+                await Init();
+                if (user.Id != 0)
+                {
+                    return await Database.UpdateAsync(user);
+                }
+                else
+                {
+                    return await Database.InsertAsync(user);
+                }
             }
-            else
+            catch (Exception ex)
             {
-                return await Database.InsertAsync(user);
+                Debug.WriteLine(ex.Message);
+                return 0;
             }
         }
         internal async Task<int> DeleteUserAsync(User user)
         {
-            await Init();
-            return await Database.DeleteAsync(user);
+            try
+            {
+                await Init();
+                return await Database.DeleteAsync(user);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+                return 0;
+            }
         }
         internal async Task<List<UserScore>> GetUserScoreAsync(string username)
         {
-            await Init();
-            var query = "SELECT users.name, highscores.score FROM users JOIN highscores ON users.id = highscores.user_id WHERE users.name = ? ORDER BY highscores.score DESC LIMIT 10";
-            List<UserScore> userScores = await Database.QueryAsync<UserScore>(query, username);
-            return userScores;
+            try
+            {
+                await Init();
+                var query = "SELECT users.name, highscores.score FROM users JOIN highscores ON users.id = highscores.user_id WHERE users.name = ? ORDER BY highscores.score DESC LIMIT 10";
+                List<UserScore> userScores = await Database.QueryAsync<UserScore>(query, username);
+                return userScores;
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+                return null;
+            }
         }
         internal async Task<int> UpdateScoresAsync(Highscore highscore)
         {
-            await Init();
-            return await Database.InsertAsync(highscore);
+            try
+            {
+                await Init();
+                return await Database.InsertAsync(highscore);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+                return 0;
+            }
         }
     }
 }
