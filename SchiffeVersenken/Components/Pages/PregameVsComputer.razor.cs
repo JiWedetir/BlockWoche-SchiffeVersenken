7e13e0ea59441cb78f40dd65dcf3f1d9abab7909
JiWedetir <serafettindiyart@hotmail.com>
2024-02-24 18:30:01 +0100

    Added Info Buttons for PregameVsComputer and ShipPlacement Page

--- SchiffeVersenken/Components/Pages/PregameVsComputer.razor.cs
+++ SchiffeVersenken/Components/Pages/PregameVsComputer.razor.cs
@@ -1,11 +1,14 @@
 ﻿using Microsoft.AspNetCore.Components;
+using MudBlazor;
+using SchiffeVersenken.Components.Shared;
 using SchiffeVersenken.Data;
 using SchiffeVersenken.Data.Controller;
+using static Microsoft.Maui.ApplicationModel.Permissions;
 
 
 namespace SchiffeVersenken.Components.Pages
 {
-    public partial class PregameVsComputer
+	public partial class PregameVsComputer
     {
         // Gets the game logic service instance, cascaded from a parent component
         [CascadingParameter]
@@ -42,5 +45,21 @@ namespace SchiffeVersenken.Components.Pages
             GameService.Game.StartPlacingShips(_FieldSize, _Difficulty);
             NavigationManager.NavigateTo("/ShipPlacement", true);
         }
-    }
+
+		/// <summary>
+		/// Opens a Info PopUp with the given text
+		/// </summary>
+		private void OpenDialogInfo()
+		{
+			string text =   $"<b>PregameVsComputer Instructions:</b>\n" +
+							$"&#8226; By dragging the slider you can choose the field size you would like. Keep in mind, the bigger the field, the longer the game will take\n" +
+							$"&#8226; By clicking on the below select field, you can choose the computer diffulty you want to play against.\n" +
+							$"&emsp;&#8226; 'Dumb' Opponent: Shoots randomly without paying attention to anything.\n" +
+							$"&emsp;&#8226; 'Klug' Opponent: Shoots randomly, but if it hits a ship, it shoots around the hit square to sink the ship.\n" +
+							$"&emsp;&#8226; 'Genie' Opponent: Like the Smart Opponent, but calculates the most likely ship position and then shoots there.\n" +
+							$"&#8226; When you want to continue to placing your ships, simply click the 'Start Placings Ships' Button.";
+			DialogParameters parameters = new DialogParameters { { "ContentText", text } };
+			DialogService.Show<InfoDialog>("", parameters);
+		}
+	}
 }
