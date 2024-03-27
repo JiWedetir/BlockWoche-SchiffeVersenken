d9d03a440190af47d981371dd120607f2e37fd49
JiWedetir <serafettindiyart@hotmail.com>
2024-03-27 15:09:56 +0100

    Changed first migration and db creation when none existent

--- SchiffeVersenken/MauiProgram.cs
+++ SchiffeVersenken/MauiProgram.cs
@@ -1,36 +1,43 @@
-﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Logging;
+﻿using Microsoft.Extensions.Logging;
 using MudBlazor.Services;
 using SchiffeVersenken.Data;
+using SchiffeVersenken.DatabaseEF.Database;
 
 namespace SchiffeVersenken
 {
-    public static class MauiProgram
-    {
-        public static MauiApp CreateMauiApp()
-        {
-            var builder = MauiApp.CreateBuilder();
-            builder
-                .UseMauiApp<App>()
-                .ConfigureFonts(fonts =>
-                {
-                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-                    fonts.AddFont("ka1.ttf", "Karmatic");
-                    fonts.AddFont("6809-chargen.ttf", "6809chargen");
-                });
+	public static class MauiProgram
+	{
+		public static MauiApp CreateMauiApp()
+		{
+			var builder = MauiApp.CreateBuilder();
+			builder
+				.UseMauiApp<App>()
+				.ConfigureFonts(fonts =>
+				{
+					fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+				});
 
-            builder.Services.AddMauiBlazorWebView();
-            builder.Services.AddMudServices();
+			builder.Services.AddMauiBlazorWebView();
+			builder.Services.AddMudServices();
 			builder.Services.AddSingleton<GameLogicService>();
-            builder.Services.AddDbContext<DatabaseContext>(options =>
-                options.UseSqlite("Data Source=schiffeversenken.db"));
+			builder.Services.AddDbContext<DatabaseContext>();
 
 #if DEBUG
 			builder.Services.AddBlazorWebViewDeveloperTools();
-    		builder.Logging.AddDebug();
+			builder.Logging.AddDebug();
 #endif
+			var app = builder.Build();
 
-            return builder.Build();
-        }
-    }
+			using (var scope = app.Services.CreateScope())
+			{
+				var dbContext = scope.ServiceProvider.GetRequiredService<DatabaseContext>();
+				if (dbContext.Database.EnsureCreated())
+				{
+					DatabaseAccess.CreateDefaultUsers().Wait();
+				}
+			}
+
+			return app;
+		}
+	}
 }
